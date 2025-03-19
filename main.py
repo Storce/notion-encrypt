ffbbaed6c3e306c39b93868128450f8d54c9528a
Joe Wang <69328902+Storce@users.noreply.github.com>
2025-03-19 13:32:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from notion_client import Client
 from dotenv import load_dotenv
+import encrypt
 
 # Load environment variables from .env file
 load_dotenv()
@@ -16,53 +17,74 @@ print(CHILD_PAGE_ID)
 # Initialize the Notion client.
 notion = Client(auth=NOTION_TOKEN)
 
-def get_child_pages(parent_id):
+def get_child_pages(parent_id: str) -> list:
     """
-    Retrieve all child pages (blocks of type "child_page") of the given parent page.
+    Retrieve all child page ids of the given parent page.
     """
     pages = []
     next_cursor = None
-    while True:
-        response = notion.blocks.children.list(
-            block_id=parent_id,
-            start_cursor=next_cursor
-        )
-        for block in response.get("results"):
-            if block.get("type") == "child_page":
-                pages.append(block)
-        if not response.get("has_more"):
-            break
-        next_cursor = response.get("next_cursor")
+    response = notion.blocks.children.list(
+            block_id=parent_id
+    )
+    for block in response.get("results"):
+        if block.get("type") == "child_page":
+            pages.append(block.get("id"))
     return pages
 
-def process_page(page_id: str):
-    next_cursor = None
-    while True:
-        response = notion.blocks.children.list(
-            block_id=page_id,
-            start_cursor=next_cursor
-        )
-        for block in response.get("results"):
-            processed_block = process_block(block)
-            print(processed_block)
-        if not response.get("has_more"):
-            break
-        next_cursor = response.get("next_cursor")
-    return None
 
-def process_block(block: dict):
-    if block.get("type") == "paragraph":
-        paragraph = block.get("paragraph")
-        if "rich_text" in paragraph and isinstance(paragraph["rich_text"], list):
-            for text_obj in block["rich_text"]:
-                # Check if the block element is of type 'text' and has a 'text' dict.
-                if text_obj.get("type") == "text" and "text" in text_obj:
-                    text_obj["text"]["content"] = "hello world"
-                    text_obj["plain_text"] = "hello world"    
-    return block
+def get_all_blocks(page_id: str) -> list:
+    """
+    Retrieve all blocks of a given page.
+    """
+    blocks = []
+    next_cursor = None
+    response = notion.blocks.children.list(
+        block_id=page_id
+    )
+    for block in response.get("results"):
+        blocks.append(block)
+    return blocks
+
+
+def process_block(blocks: list) -> list:
+    """
+    Process and encrypt a list of blocks. Return the modified list.
+    """
+    for block in blocks:
+        if block.get("type") == "paragraph":
+            paragraph = block.get("paragraph")
+            for content in paragraph["rich_text"]:
+                encrypted_text = encrypt.encrypt(content['text']['content'])
+                content['text']['content'] = encrypted_text
+                content['plain_text'] = encrypted_text
+
+    return blocks
+
+
+def get_title(page_id: str) -> str:
+    """
+    Retrieve the title of a page.
+    """
+    response = notion.pages.retrieve(page_id=page_id)
+    return response.get("properties").get("title").get("title")[0].get("text").get("content")
+
+
+def create_page(blocks: list, parent_id: str, title: str) -> bool:
+    """
+    Create a new page with the given blocks under the specified parent.
+    """
+    new_page = notion.pages.create(
+        parent={"type": "page_id", "page_id": parent_id},
+        children=blocks,
+        properties={"title": {"title": [{"type": "text", "text": {"content": "gib." + title}}]}},
+    )
+    return True
+
 
 def main():
-    process_page(CHILD_PAGE_ID)
+    for page_id in get_child_pages(PARENT_PAGE_ID):
+        processed = process_block(get_all_blocks(page_id))
+        create_page(processed, PARENT_PAGE_ID, get_title(page_id))
 
 if __name__ == "__main__":
     main()
